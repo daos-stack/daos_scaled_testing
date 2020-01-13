9fa054ffca008b0927ce020c46727c4faefd7e76
Sylvia Oi Yee Chan <sylvia.oi.yee.chan@intel.com>
2020-01-13 17:14:16 +0000

    DAOS-3943 Enable selftest to run with test_group
    Enable selftest to run with test_group
    
    Signed-off-by: Sylvia Oi Yee Chan <sylvia.oi.yee.chan@intel.com>

--- run_cart.sh
+++ run_cart.sh
@@ -15,8 +15,8 @@
 #SBATCH --mail-type=all         # Send email at begin and end of job
 
 #Parameter to be updated for each sbatch
-DAOS_SERVERS=2
-DAOS_CLIENTS=1
+N_SERVERS=2
+N_CLIENTS=1
 MAX_INFLIGHT=16
 CARTDIR="/home1/<PATH_TO_CART>/cart"
 
@@ -49,6 +49,7 @@ function wait {
         echo "" 
 }
 
+# run_server np cmd
 function run_bg {
         echo "CMD:"  
         echo orterun --map-by node --timeout $timeout --mca mtl ^psm2,ofi -x FI_PSM2_DISCONNECT=1 --np $1 --hostfile $2 --output-filename testLogs/$4/sep_$test_$sep -x D_LOG_FILE=testLogs/$4/sep_$test_$sep/srv_output.log -x D_LOG_MASK=ERR -x CRT_PHY_ADDR_STR=ofi+psm2 -x OFI_INTERFACE=$interface -x CRT_CTX_SHARE_ADDR=$sep -x CRT_CTX_NUM=16 -x PATH -x LD_LIBRARY_PATH $3
@@ -60,6 +61,7 @@ function run_bg {
         wait 5
 }
 
+# run_server np cmd
 function run_fg {
         echo "CMD:"
         echo orterun --map-by node --timeout $timeout --mca mtl ^psm2,ofi -x FI_PSM2_DISCONNECT=1 --np $1 --hostfile $2 --output-filename testLogs/$4/sep_$test_$sep -x D_LOG_FILE=testLogs/$4/sep_$test_$sep/srv_output.log -x D_LOG_MASK=ERR -x CRT_PHY_ADDR_STR=ofi+psm2 -x OFI_INTERFACE=$interface -x CRT_CTX_SHARE_ADDR=$sep -x CRT_CTX_NUM=16 -x PATH -x LD_LIBRARY_PATH $3
@@ -78,6 +80,7 @@ function run_fg {
         echo ""
 }
 
+# run_client np cmd
 function run_st {
         echo "CMD:"  
         echo orterun --map-by node --timeout $timeout --mca mtl ^psm2,ofi -x FI_PSM2_DISCONNECT=1 --np $1 --hostfile $2 --output-filename testLogs/$4/sep_$test_$sep -x D_LOG_FILE=testLogs/$4/sep_$test_$sep/srv_output.log -x D_LOG_MASK=ERR -x CRT_PHY_ADDR_STR=ofi+psm2 -x OFI_INTERFACE=$interface -x CRT_CTX_SHARE_ADDR=$sep -x CRT_CTX_NUM=16 -x PATH -x LD_LIBRARY_PATH $3 --message-sizes "b1048576,b1048576 0,0 b1048576,i2048,i2048 0,0 i2048,0"
@@ -101,20 +104,15 @@ prepare(){
     #Create the folder for server/client logs.
     mkdir -p Log/$SLURM_JOB_ID
 
-    #Prepare DAOS server list file
-    srun -n $SLURM_JOB_NUM_NODES hostname > Log/$SLURM_JOB_ID/slurm_hostlist
-    sed -i "/$HOSTNAME/d" Log/$SLURM_JOB_ID/slurm_hostlist
-    cat Log/$SLURM_JOB_ID/slurm_hostlist | tail -$DAOS_SERVERS > Log/$SLURM_JOB_ID/slurm_server_hostlist
-    sed 's/$/ slots=1/' Log/$SLURM_JOB_ID/slurm_server_hostlist > Log/$SLURM_JOB_ID/daos_server_hostlist
-    cat Log/$SLURM_JOB_ID/slurm_hostlist | head -$DAOS_CLIENTS > Log/$SLURM_JOB_ID/daos_client_hostlist
+    ./gen_hostlist.sh $N_SERVERS $N_CLIENTS
 }
 
 run_selftest(){
-    last_srv_index=$(( DAOS_SERVERS-1 ))
-    run_bg $DAOS_SERVERS Log/$SLURM_JOB_ID/daos_server_hostlist "${CARTDIR}/install/Linux/bin/crt_launch -e ${CARTDIR}/install/Linux/TESTING/tests/test_group_np_srv --name selftest_srv_grp_$DAOS_SERVERS --cfg_path=${CARTDIR}/install/Linux/TESTING" $test
+    last_srv_index=$(( $N_SERVERS-1 ))
+    run_bg $N_SERVERS Log/$SLURM_JOB_ID/daos_server_hostlist "${CARTDIR}/install/Linux/bin/crt_launch -e ${CARTDIR}/install/Linux/TESTING/tests/test_group_np_srv --name selftest_srv_grp_$N_SERVERS --cfg_path=${CARTDIR}/install/Linux/TESTING" $test
     wait 5
-    run_st $DAOS_CLIENTS Log/$SLURM_JOB_ID/daos_client_hostlist "${CARTDIR}/install/Linux/bin/self_test --group-name selftest_srv_grp_$DAOS_SERVERS --endpoint 0-$last_srv_index:0 --master-endpoint 0:0 --max-inflight-rpcs $MAX_INFLIGHT --repetitions 100 -t -n -p ${CARTDIR}/install/Linux/TESTING" $test
-    run_fg $DAOS_CLIENTS Log/$SLURM_JOB_ID/daos_client_hostlist "${CARTDIR}/install/Linux/TESTING/tests/test_group_np_cli --name client-group --attach_to selftest_srv_grp_$DAOS_SERVERS --shut_only --cfg_path=${CARTDIR}/install/Linux/TESTING" $test
+    run_st $N_CLIENTS Log/$SLURM_JOB_ID/daos_client_hostlist "${CARTDIR}/install/Linux/bin/self_test --group-name selftest_srv_grp_$N_SERVERS --endpoint 0-$last_srv_index:0 --master-endpoint 0:0 --max-inflight-rpcs $MAX_INFLIGHT --repetitions 100 -t -n -p ${CARTDIR}/install/Linux/TESTING" $test
+    run_fg $N_CLIENTS Log/$SLURM_JOB_ID/daos_client_hostlist "${CARTDIR}/install/Linux/TESTING/tests/test_group_np_cli --name client-group --attach_to selftest_srv_grp_$N_SERVERS --shut_only --cfg_path=${CARTDIR}/install/Linux/TESTING" $test
 }
 
 for test in "$@"; do
@@ -122,26 +120,27 @@ for test in "$@"; do
     case $test in
         sanity)
             prepare
-            run_fg $DAOS_SERVERS Log/$SLURM_JOB_ID/daos_server_hostlist "hostname" $test
+            run_fg $N_SERVERS Log/$SLURM_JOB_ID/daos_server_hostlist "hostname" $test
             ;;
         barrier)
             prepare
-            run_fg $DAOS_SERVERS Log/$SLURM_JOB_ID/daos_server_hostlist "${CARTDIR}/install/Linux/bin/crt_launch -e ${CARTDIR}/install/Linux/TESTING/tests/test_crt_barrier" $test
+            run_fg $N_SERVERS Log/$SLURM_JOB_ID/daos_server_hostlist "${CARTDIR}/install/Linux/bin/crt_launch -e ${CARTDIR}/install/Linux/TESTING/tests/test_crt_barrier" $test
             ;;
         self_test)
-            let DAOS_SERVER=2
-            until [  $DAOS_SERVERS -gt 65 ]; do
+            let $N_SERVER=2
+            until [  $N_SERVERS -gt 65 ]; do
                 prepare
                 run_selftest
-                let DAOS_SERVERS=$(( DAOS_SERVERS*2 ))
+                let $N_SERVERS=$(( $N_SERVERS*2 ))
             done
 
-            let DAOS_SERVERS=126
+            let $N_SERVERS=126
             prepare
             run_selftest
             ;;
     esac
 done
 
-mv stderr* stdout* testLogs Log/$SLURM_JOB_ID
+mv *$SLURM_JOB_ID testLogs Log/$SLURM_JOB_ID
+
 
