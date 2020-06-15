aacb1144cf1205e03d976048351dd9af6dcddf8c
Sylvia Chan <soychan@login4.frontera.tacc.utexas.edu>
2020-06-15 16:48:10 -0500

    Enable ior and mdtest to run with daos master
    Launching daos_server and daos_agent with clush
    Switching from orterun to mpich for launching client apps
    
    Signed-off-by: Sylvia Chan <soychan@login4.frontera.tacc.utexas.edu>

--- gen_hostlist.sh
+++ gen_hostlist.sh
@@ -3,8 +3,6 @@
 N_SERVERS=$1
 N_CLIENTS=$2
 
-srun -n $SLURM_JOB_NUM_NODES hostname > Log/$SLURM_JOB_ID/slurm_hostlist
-sed -i "/$(hostname)/d" Log/$SLURM_JOB_ID/slurm_hostlist
-cat Log/$SLURM_JOB_ID/slurm_hostlist | tail -$N_SERVERS > Log/$SLURM_JOB_ID/slurm_server_hostlist
-sed 's/$/ slots=1/' Log/$SLURM_JOB_ID/slurm_server_hostlist > Log/$SLURM_JOB_ID/daos_server_hostlist
-cat Log/$SLURM_JOB_ID/slurm_hostlist | head -$N_CLIENTS > Log/$SLURM_JOB_ID/daos_client_hostlist
+srun -n $SLURM_JOB_NUM_NODES hostname | sed "/$(hostname)/d" | cut -c 1-8 > Log/$SLURM_JOB_ID/daos_all_hostlist
+cat Log/$SLURM_JOB_ID/daos_all_hostlist | tail -$N_SERVERS > Log/$SLURM_JOB_ID/daos_server_hostlist
+cat Log/$SLURM_JOB_ID/daos_all_hostlist | head -$N_CLIENTS > Log/$SLURM_JOB_ID/daos_client_hostlist
--- main.sh
+++ main.sh
@@ -8,9 +8,9 @@
 #SBATCH -e stderr.e%j           # Name of stderr error file
 #SBATCH -A STAR-Intel           # Project Name
 #SBATCH -p development          # Queue (partition) name
-#SBATCH -N 5                    # Total # of nodes
-#SBATCH -n 280                  # Total # of mpi tasks (56 x  Total # of nodes)
-#SBATCH -t 00:30:00             # Run time (hh:mm:ss)
+#SBATCH -N 6                    # Total # of nodes
+#SBATCH -n 336                  # Total # of mpi tasks (56 x  Total # of nodes)
+#SBATCH -t 00:15:00             # Run time (hh:mm:ss)
 #SBATCH --mail-user=<first.last>@intel.com
 #SBATCH --mail-type=all         # Send email at begin and end of job
 
@@ -18,7 +18,7 @@
 DAOS_SERVERS=4
 DAOS_CLIENTS=1
 ACCESS_PORT=10001
-DAOS_DIR="<PATH_TO_DAOS>/daos"
+DAOS_DIR="<path_to_daos>/daos"
 POOL_SIZE="42G"
 
 #IOR Parameter
@@ -28,23 +28,18 @@ BL_SIZE="64M"
 
 #Cart Self test parameter
 ST_MAX_INFLIGHT=(1 16)
-ST_PARAMS="-x FI_MR_CACHE_MAX_COUNT=0"
 ST_MIN_SRV=2
 ST_MAX_SRV=$(( $DAOS_SERVERS ))
-ST_TIMEOUT=1800
 
 #Others
 SRUN_CMD="srun -n $SLURM_JOB_NUM_NODES -N $SLURM_JOB_NUM_NODES"
 DAOS_SERVER_YAML="$PWD/daos_server.yml"
 DAOS_AGENT_YAML="$PWD/daos_agent.yml"
-VERBS_CLIENT_PARAM=" --mca btl_openib_warn_default_gid_prefix 0 --mca pml ob1 --mca btl tcp,self --mca oob tcp"
+DAOS_CONTROL_YAML="$PWD/daos_control.yml"
 
 HOSTNAME=$(hostname)
 echo $HOSTNAME
 source env_daos $DAOS_DIR
-
-export PATH=~/utils/install/bin:$PATH
-export LD_LIBRARY_PATH=~/utils/install/lib:$LD_LIBRARY_PATH
 
 #Collect logs from all servers/clients
 collect_logs(){
@@ -63,8 +58,7 @@ killall_proc(){
 #Cleanup the SCM content and kill the processes.
 cleanup (){
     mv *$SLURM_JOB_ID Log/$SLURM_JOB_ID/
-    cp $DAOS_SERVER_YAML $DAOS_AGENT_YAML Log/$SLURM_JOB_ID/
-    mv testLogs Log/$SLURM_JOB_ID/
+    cp $DAOS_SERVER_YAML $DAOS_AGENT_YAML $DAOS_CONTROL_YAML Log/$SLURM_JOB_ID/
     killall_proc "cleanup"
 }
 
@@ -78,10 +72,11 @@ prepare(){
 
     ./gen_hostlist.sh $DAOS_SERVERS $DAOS_CLIENTS
 
-    ACCESS_POINT=`cat Log/$SLURM_JOB_ID/slurm_server_hostlist | head -1 | grep -o -m 1 "^c[0-9\-]*"`
+    ACCESS_POINT=`cat Log/$SLURM_JOB_ID/daos_server_hostlist | head -1`
 
     sed -i "/^access_points/ c\access_points: ['$ACCESS_POINT:$ACCESS_PORT']" $DAOS_SERVER_YAML
     sed -i "/^access_points/ c\access_points: ['$ACCESS_POINT:$ACCESS_PORT']" $DAOS_AGENT_YAML
+    sed -i "s/^\- .*/\- $ACCESS_POINT:$ACCESS_PORT/" $DAOS_CONTROL_YAML
 
     #Create the daos_agent folder
     srun -n $SLURM_JOB_NUM_NODES mkdir  /tmp/daos_agent
@@ -96,7 +91,10 @@ prepare_test_log_dir(){
 #Start DAOS agent
 start_agent(){
     echo -e "\nCMD: Starting agent...\n"
-    cmd="srun -n $SLURM_JOB_NUM_NODES $DAOS_DIR/install/bin/daos_agent -i -o $DAOS_AGENT_YAML -s /tmp/daos_agent"
+    cmd="clush --hostfile Log/$SLURM_JOB_ID/daos_all_hostlist \"
+    export PATH=$PATH; export LD_LIBRARY_PATH=$LD_LIBRARY_PATH; export CPATH=$CPATH;
+    export DAOS_DISABLE_REQ_FWD=1;
+    daos_agent -i -o $DAOS_AGENT_YAML -s /tmp/daos_agent\" "
     echo $cmd
     echo
     eval $cmd &
@@ -116,7 +114,7 @@ dump_attach_info(){
 #Create Pool
 create_pool(){
     echo -e "\nCMD: Creating pool\n"
-    cmd="dmg -l $ACCESS_POINT:$ACCESS_PORT -i pool create --scm-size $POOL_SIZE"
+    cmd="dmg -o $DAOS_CONTROL_YAML -i pool create --scm-size $POOL_SIZE"
     echo $cmd
     echo
     DAOS_POOL=`$cmd`
@@ -138,28 +136,17 @@ create_pool(){
 #Start daos servers
 start_server(){
     echo -e "\nCMD: Starting server...\n"
-    rm -r server_output.log
-    cmd="orterun  --np $DAOS_SERVERS --hostfile Log/$SLURM_JOB_ID/daos_server_hostlist -x CPATH -x PATH -x LD_LIBRARY_PATH 
-    --enable-recovery daos_server start -i -o $DAOS_SERVER_YAML  
-    --recreate-superblocks 2>&1 | tee server_output.log"
+    cmd="clush --hostfile Log/$SLURM_JOB_ID/daos_server_hostlist \"
+    export PATH=$PATH; export LD_LIBRARY_PATH=$LD_LIBRARY_PATH; export CPATH=$CPATH;
+    export DAOS_DISABLE_REQ_FWD=1;
+    daos_server start -i -o $DAOS_SERVER_YAML  
+    --recreate-superblocks
+    \" 2>&1 "
     echo $cmd
     echo
     eval $cmd &
-    TIMEOUT=0
-    while :
-        do
-        VAL=`cat server_output.log | grep "started on rank" | wc -l`
-        if (($TIMEOUT > 30)); then
-            echo "FAIL: Failed to start DAOS servers"
-            exit 1
-        elif (($DAOS_SERVERS == $VAL));then
-            break
-        else
-            echo "Waiting to start all servers....."
-            sleep 10
-            let "TIMEOUT++"
-        fi
-    done
+
+    sleep 60;
 }
 
 #Run IOR
@@ -169,9 +156,13 @@ run_IOR(){
         for i in "${IOR_PROC_PER_CLIENT[@]}"; do
             no_of_ps=$(($DAOS_CLIENTS * $i))
 	    echo
-            cmd="orterun --timeout 600 $VERBS_CLIENT_PARAM 
-            -np $no_of_ps --map-by node  --hostfile Log/$SLURM_JOB_ID/daos_client_hostlist ior -a DAOS -b $BL_SIZE  -w -r -i 3 -o daos:testFile 
-            -t $size --daos.cont $(uuidgen) --daos.destroy --daos.group daos_server --daos.pool $POOL_UUID  --daos.svcl $POOL_SVC -vv"
+            cmd="mpirun -np $no_of_ps -map-by node 
+                 -hostfile Log/$SLURM_JOB_ID/daos_client_hostlist
+                 ior
+                 -a DAOS -b $BL_SIZE  -w -r -i 3 -o daos:testFile 
+                 -t $size --daos.cont $(uuidgen) --daos.destroy
+                 --daos.group daos_server --daos.pool $POOL_UUID
+                 --daos.svcl $POOL_SVC -vv"
             echo $cmd
 	    echo
             eval $cmd
@@ -186,23 +177,6 @@ run_IOR(){
     done
 }
 
-#Run daos_test
-run_daos_test(){
-    echo -e "\nCMD: Starting daos_test...\n"
-    #Run daos_test from single client for now.
-    cmd="orterun --timeout 1800 -np 1 --map-by node
-    --hostfile Log/$SLURM_JOB_ID/daos_client_hostlist daos_test -p"
-    echo $cmd
-    echo
-    eval $cmd
-    if [ $? -ne 0 ]; then
-        echo -e "\nSTATUS: daos_test FAIL\n"
-	exit 1
-    else
-        echo -e "\nSTATUS: daos_test SUCCESS\n"
-    fi
-}
-
 #Run cart self_test
 run_cart_test(){
     echo -e "\nCMD: Starting CaRT self_test...\n"
@@ -211,11 +185,8 @@ run_cart_test(){
 	let last_srv_index=$(( ${ST_MIN_SRV}-1 ))
 
         for max_inflight in "${ST_MAX_INFLIGHT[@]}"; do
-            cmd="orterun --timeout ${ST_TIMEOUT} -np 1 ${VERBS_CLIENT_PARAM} 
-                 --map-by node  --hostfile Log/${SLURM_JOB_ID}/daos_client_hostlist
-	         --output-filename testLogs/srv${ST_MIN_SRV}_inf${max_inflight}
-	         -x PATH -x LD_LIBRARY_PATH ${ST_PARAMS}
-		 -x D_LOG_FILE=testLogs/srv${ST_MIN_SRV}_inf${max_inflight}/output.log
+            cmd="mpirun -prepend-rank -np 1 -map-by node
+                 -hostfile Log/${SLURM_JOB_ID}/daos_client_hostlist
 	         self_test
 	         --group-name daos_server --endpoint 0-${last_srv_index}:0
 	         --message-sizes 'b1048576',' b1048576 0','0 b1048576',' b1048576 i2048',' i2048 b1048576',' i2048',' i2048 0','0 i2048','0' 
@@ -234,6 +205,8 @@ run_cart_test(){
 
 	let ST_MIN_SRV=$(( ${ST_MIN_SRV}*2 ))
     done
+
+    mv daos_server.attach_info_tmp Log/$SLURM_JOB_ID
 }
 
 run_mdtest(){
@@ -241,9 +214,12 @@ run_mdtest(){
     for i in "${IOR_PROC_PER_CLIENT[@]}"; do
 	no_of_ps=$(($DAOS_CLIENTS * $i))
 	echo
-        cmd="orterun --timeout 600 $VERBS_CLIENT_PARAM -np $no_of_ps --map-by node
-        --hostfile Log/$SLURM_JOB_ID/daos_client_hostlist mdtest -a DFS  --dfs.destroy --dfs.pool $POOL_UUID 
-        --dfs.cont $(uuidgen) --dfs.svcl $POOL_SVC -n 500  -u -L --dfs.oclass S1 -N 1 -P -d /"
+        cmd="mpirun -np $no_of_ps -map-by node
+             -hostfile Log/$SLURM_JOB_ID/daos_client_hostlist
+             mdtest
+             -a DFS --dfs.destroy --dfs.pool $POOL_UUID 
+             --dfs.cont $(uuidgen) --dfs.svcl $POOL_SVC
+             -n 500  -u -L --dfs.oclass S1 -N 1 -P -d /"
         echo $cmd
 	echo
         eval $cmd
@@ -266,19 +242,11 @@ for test in "$@"; do
     prepare_test_log_dir $test
     case $test in
         IOR)
-            #start_server
-            #start_agent
-	    #create_pool
-            #run_IOR
-            #killall_proc $test
-	    echo "WIP to enable IOR on daos master. Please use older commit for running IOR on 1.0.0"
-            ;;
-        DAOS_TEST)
-            #start_server
-            #start_agent
-            #run_daos_test
-            #killall_proc $test
-	    echo "DAOS_TEST is temporarily disabled - TBD"
+            start_server
+            start_agent
+	    create_pool
+            run_IOR
+            killall_proc $test
             ;;
         SELF_TEST)
             start_server
@@ -287,12 +255,11 @@ for test in "$@"; do
             killall_proc $test
             ;;
         MDTEST)
-            #start_server
-            #start_agent
-            #create_pool
-            #run_mdtest
-            #killall_proc $test
-	    echo "WIP to enable MDTEST on daos master. Please use older commit for running MDTEST on 1.0.0"
+            start_server
+            start_agent
+            create_pool
+            run_mdtest
+            killall_proc $test
             ;;  
         *)
             echo "Unknown test: Please use IOR DAOS_TEST SELF_TEST or MDTEST"
