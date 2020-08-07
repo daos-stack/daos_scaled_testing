3c0c6e7f2ecdf548efaf166769853adfa41d1c31
hpcarchitect <54078405+hpcarchitect@users.noreply.github.com>
2020-08-07 16:05:51 -0400

    Add files via upload
    Simplified the scripts to operate on a single run output per configuration.

--- get_ior_results.sh
+++ get_ior_results.sh
@@ -16,57 +16,14 @@ cd $RES_DIR
 
 #Test name
 current=${PWD##*/}
+echo $current
 
-#Output files - raw* for extracting the results from tests and result* for final results in csv format into a file ready for plotting
-raw="./raw_$current"
-result="./result_$current"
-
-process_result() {
-    infile=$1
-    outfile=$2
-    if [ -f ./tmp ] ; then rm ./tmp ; fi
-    echo "Num_Servers,Write(GiB/s),Read(GiB/s),Write(MiB/s),Read(MiB/s)" > $outfile
-    {
-        last=0
-        {
-        read
-        while  read -r line ; do
-            cur=`awk -F, '{print $1}' <<< "$line"`
-            wr=`awk -F, '{print $5}' <<< "$line"`
-            rd=`awk -F, '{print $6}' <<< "$line"`
-            if  [[ $cur -ne $last ]] ; then
-                calc_averages  $cnt  
-                last=$cur
-                cnt=1
-                sum_wr=$wr
-                sum_rd=$rd
-            else
-                let "cnt++"
-                sum_wr=`echo "$sum_wr + $wr" | bc`
-                sum_rd=`echo "$sum_rd + $rd" | bc`
-            fi
-        done
-        }  < "$infile"
-        calc_averages  $cnt 
-        echo $infile $outfile
-    }
-    cat tmp | tee -a  $outfile
-}
-
-calc_averages () {
-    num=$1
-    if [[  $last -ne "0" ]] ; then
-    av_wr=`echo "scale=2;$sum_wr / $num" | bc`
-    av_rd=`echo "scale=2;$sum_rd / $num" | bc`
-    wr_GiB=`echo "scale=2;$av_wr / 1024" | bc`
-    rd_GiB=`echo "scale=2;$av_rd / 1024" | bc`
-    echo "$last","$wr_GiB","$rd_GiB","$av_wr","$av_rd" >> tmp
-    fi
-}
+#Output file
+result="$RES_DIR/result_$current"
 
 # Extract results from test logs
 
-echo Num_Servers,Targets,Clients,Ranks,Write-Easy,Read-Easy > $raw
+echo Num_Servers,Targets,Clients,Ranks,Write-Easy,Read-Easy > $result
 
 for i in *
 do
@@ -79,15 +36,10 @@ do
            ranks=`awk '{print $8}' <<< "$str"`
            wr=`grep  "Max Write" ./$i/ior* | awk '{print $3}'`
            rd=`grep "Max Read" ./$i/ior* | awk '{print $3}'`
-           echo "$servers","$targets","$clients","$ranks","$wr","$rd" >> $raw
+           wr_GiB=`echo "scale=2;$wr / 1024" | bc`
+           rd_GiB=`echo "scale=2;$rd / 1024" | bc`
+           echo "$servers","$wr_GiB","$rd_GiB","$wr","$rd" >> $result
        fi
     fi
 
 done
-
-#Generate result files in plot firendly format
-
-process_result $raw $result
-
-#cleanup
-rm ./tmp
--- get_mdtest_results.sh
+++ get_mdtest_results.sh
@@ -17,66 +17,12 @@ cd $RES_DIR
 #Test name
 current=${PWD##*/}
 
-#Output files - raw* for extracting the results from tests and result* for final results in csv format into a file ready for plotting
-raw="./raw_$current"
-result="./result_$current"
-
-process_result() {
-    infile=$1
-    outfile=$2
-    if [ -f ./tmp ] ; then rm ./tmp ; fi
-    echo "Num_Servers,create(Kops/sec),stat(Kops/sec),read(Kops/sec),rm(Kops/sec),creates/sec,stat/sec,reads/sec,rms/sec" > $outfile
-    {
-        last=0
-        {
-        read
-        while  read -r line ; do
-            cur=`awk -F, '{print $1}' <<< "$line"`
-            cr=`awk -F, '{print $5}' <<< "$line"`
-            st=`awk -F, '{print $6}' <<< "$line"`
-            rd=`awk -F, '{print $7}' <<< "$line"`
-            rm=`awk -F, '{print $8}' <<< "$line"`
-            if  [[ $cur -ne $last ]] ; then
-                calc_averages  $cnt
-                last=$cur
-                cnt=1
-                sum_cr=$cr
-                sum_st=$st
-                sum_rd=$rd
-                sum_rm=$rm
-            else
-                let "cnt++"
-                sum_cr=`echo "$sum_cr + $cr" | bc`
-                sum_st=`echo "$sum_st + $st" | bc`
-                sum_rd=`echo "$sum_rd + $rd" | bc`
-                sum_rm=`echo "$sum_rm + $rm" | bc`
-            fi
-        done
-        }  < $infile
-        calc_averages  $cnt
-        echo 
-    }
-    cat tmp | tee -a  $outfile
-}
-
-calc_averages () {
-    num=$1
-    if [[  $last -ne "0" ]] ; then
-    av_cr=`echo "scale=2;$sum_cr / $num" | bc`
-    av_st=`echo "scale=2;$sum_st / $num" | bc`
-    av_rd=`echo "scale=2;$sum_rd / $num" | bc`
-    av_rm=`echo "scale=2;$sum_rm / $num" | bc`
-    cr_Kop=`echo "scale=2;$av_cr / 1000" | bc`
-    st_Kop=`echo "scale=2;$av_st / 1000" | bc`
-    rd_Kop=`echo "scale=2;$av_rd / 1000" | bc`
-    rm_Kop=`echo "scale=2;$av_rm / 1000" | bc`
-    echo "$last","$cr_Kop","$st_Kop","$rd_Kop","$rm_Kop","$av_cr","$av_st","$av_rd","$av_rm" >> tmp
-    fi
-}
+#Output file
+result="$RES_DIR/result_$current"
 
 # Extract results from test logs
 
-echo Num_Servers,Targets,Clients,Ranks,create/sec,stat/sec,read/sec,remove/sec >> $raw
+echo "Num_Servers,create(Kops/sec),stat(Kops/sec),read(Kops/sec),remove(Kops/sec),creates/sec,stat/sec,reads/sec,remove/sec" > $result
 
 for i in *
 do
@@ -87,15 +33,18 @@ do
             targets=`awk '{print $4}' <<< "$str"`
             clients=`awk '{print $6}' <<< "$str"`
             ranks=`awk '{print $8}' <<< "$str"`
-            output=`grep -A 6 "SUMMARY rate:" ./$i/mdtest* | grep -A 3 "File creation" | awk '{print $6}' | paste -d, - - - - `
-            echo "$servers","$targets","$clients","$ranks","$output" >> $raw
+
+            cr=`grep -A 6 "SUMMARY rate:" ./$i/mdtest* | grep "File creation" | awk '{print $6}'`
+            st=`grep -A 6 "SUMMARY rate:" ./$i/mdtest* | grep "File stat" | awk '{print $6}'`
+            rd=`grep -A 6 "SUMMARY rate:" ./$i/mdtest* | grep "File read" | awk '{print $6}'`
+            rl=`grep -A 6 "SUMMARY rate:" ./$i/mdtest* | grep "File removal" | awk '{print $6}'`
+
+            cr_Kop=`echo "scale=2;$cr / 1000" | bc`
+            st_Kop=`echo "scale=2;$st / 1000" | bc`
+            rd_Kop=`echo "scale=2;$rd / 1000" | bc`
+            rm_Kop=`echo "scale=2;$rl / 1000" | bc`
+
+            echo "$servers","$cr_Kop","$st_Kop","$rd_Kop","$rm_Kop","$cr","$st","$rd","$rl" >> $result
         fi
     fi
 done
-
-#Generate result files in plot firendly format
-
-process_result $raw $result
-
-#cleanup
-rm ./tmp
