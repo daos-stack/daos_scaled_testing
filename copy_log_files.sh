e17b887b53924cb085d32afd8342cb3c5f0bbe59
Martinez Montes, Jonathan <jonathan.martinez.montes@intel.com>
2021-02-12 14:54:37 -0600

    Add function to check clock-drift
    Signed-off-by: Martinez Montes, Jonathan <jonathan.martinez.montes@intel.com>

--- copy_log_files.sh
+++ copy_log_files.sh
@@ -2,5 +2,4 @@
 
 HOSTNAME=$(hostname)
 TMP="$HOSTNAME"
-cp -rfv /tmp/daos*log* ${RUN_DIR}/${SLURM_JOB_ID}/$1/${TMP}/ || true
 dmesg > ${RUN_DIR}/${SLURM_JOB_ID}/$1/${TMP}/dmesg_output.txt
--- create_log_dir.sh
+++ create_log_dir.sh
@@ -3,3 +3,7 @@
 HOSTNAME=$(hostname)
 TMP="$HOSTNAME"
 mkdir -p ${RUN_DIR}/${SLURM_JOB_ID}/$1/${TMP}
+
+pushd /tmp
+ln -s ${RUN_DIR}/${SLURM_JOB_ID}/$1/${TMP} daos_logs
+popd
