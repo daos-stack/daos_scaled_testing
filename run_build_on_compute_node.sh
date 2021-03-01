fbbc449111f893ccd757436d401b35dbf8ea5bdb
Martinez Montes, Jonathan <jonathan.martinez.montes@intel.com>
2021-03-01 16:18:57 -0600

    Minor fixes and enhancements
    * Propagate environment variables while running commands
      with clush.
    * Add script to build DAOS on a compute node.
    
    Signed-off-by: Martinez Montes, Jonathan <jonathan.martinez.montes@intel.com>

---
+++ run_build_on_compute_node.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+JOBNAME="<sbatch_jobname>"
+EMAIL="<email>" #<first.last@email.com>
+export BUILD_DIR="<path_build_area>" #e.g./scratch/POC/BUILDS/
+export DST_DIR="<path_to_daos_scaled_testing>" #/scratch/TESTS/daos_scaled_testing
+
+mkdir -p ${BUILD_DIR}/slurm_logs
+pushd ${BUILD_DIR}/slurm_logs
+sbatch -J ${JOBNAME} -t 01:00:00 --mail-user=${EMAIL} -N 1 -n 32 -p normal ${DST_DIR}/run_build_sbatch.sh
+popd
+
---
+++ run_build_sbatch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#SBATCH -o stdout.o%j           # Name of stdout output file
+#SBATCH -e stderr.e%j           # Name of stderr error file
+#SBATCH -A STAR-Intel           # Project Name
+#SBATCH --mail-type=all         # Send email at begin and end of job
+
+${DST_DIR}/run_build.sh "-j32" |& tee build_output_${SLURM_JOB_ID}.txt
