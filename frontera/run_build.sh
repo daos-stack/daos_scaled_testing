a37308455dfdba8392695534556ae1ac87e030b6
Dalton Bohning <dalton.bohning@intel.com>
2022-01-19 13:43:44 -0600

    parameterize run_build.sh
    - run_build.sh
      - Allow passing parameters at the command line.
      - Convert all core logic to functions
      - New hack branch
    
    Signed-off-by: Dalton Bohning <dalton.bohning@intel.com>

--- frontera/run_build.sh
+++ frontera/run_build.sh
@@ -3,57 +3,108 @@
 # Build DAOS locally.
 #
 
-# Extra arguments to pass to DAOS scons
-EXTRA_BUILD="${1}"
-
-# Directory to build in
-export BUILD_DIR="${WORK}/BUILDS/"
-
-# Only if building with mpich or openmpi
-export MPICH_DIR="${WORK}/TOOLS/mpich" # Path to locally built mpich
-export OPENMPI_DIR="${WORK}/TOOLS/openmpi" # Path to locally built openmpi
-
-# DAOS branch to clone
+# Default params
+BUILD_DIR="${WORK}/BUILDS/"
+TIMESTAMP=$(date +%Y%m%d)
 DAOS_BRANCH="master"
-
-# Optional DAOS commit to checkout after clone
 DAOS_COMMIT=""
-
-# Optional DAOS branches to merge
-DAOS_BRANCHES=()
-
-# Optional DAOS commits to cherry-pick
-DAOS_CHERRY=()
-
-# DAOS scons build type
+EXTRA_DAOS_BRANCHES=()
+EXTRA_DAOS_CHERRY=()
 DAOS_BUILD_TYPE="release"
-
-# MPI to build DAOS with
 MPI_TARGET="mvapich2"
-
-# Optional IOR commit to checkout after clone
 IOR_COMMIT=""
+SCONS_BUILD_DEPS="yes"
+SCONS_EXTRA_ARGS=""
+export MPICH_DIR="${WORK}/TOOLS/mpich"
+export OPENMPI_DIR="${WORK}/TOOLS/openmpi"
 
-# Unload modules that are not needed on Frontera
-module unload impi pmix hwloc || exit
-module list || exit
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --help|-h)
+      echo "Usage: $0"
+      echo ""
+      echo "  --build-dir=${BUILD_DIR}"
+      echo "    Directory to build in"
+      echo ""
+      echo "  --timestamp=${TIMESTAMP}"
+      echo "    Build will be in <build-dir>/<timestamp>"
+      echo "    **To be improved**"
+      echo ""
+      echo "  --daos-branch=${DAOS_BRANCH}"
+      echo "    DAOS branch to clone"
+      echo ""
+      echo "  --daos-commit=${DAOS_COMMIT}"
+      echo "    Optional DAOS commit to checkout after clone"
+      echo ""
+      echo "  --extra-daos-branches=${EXTRA_DAOS_BRANCHES}"
+      echo "    Optional DAOS branches to merge"
+      echo ""
+      echo "  --extra-daos-cherry=${EXTRA_DAOS_CHERRY}"
+      echo "    Optional DAOS commits to cherry-pick"
+      echo ""
+      echo "  --daos-build-type=${DAOS_BUILD_TYPE}"
+      echo "    DAOS scons build type"
+      echo ""
+      echo "  --mpi-target=${MPI_TARGET}"
+      echo "    MPI to build DAOS and ior with"
+      echo ""
+      echo "  --ior-commit=${IOR_COMMIT}"
+      echo "    Optional IOR commit to checkout after clone"
+      echo ""
+      echo "  --mpich-dir=${MPICH_DIR}"
+      echo "    Path to locally built MPICH, if MPI-TARGET=mpich"
+      echo ""
+      echo "  --openmpi-dir=${OPENMPI_DIR}"
+      echo "    Path to locally built OPENMPI, if MPI-TARGET=openmpi"
+      echo ""
+      echo "  --scons-build-deps=${SCONS_BUILD_DEPS}"
+      echo "    Whether DAOS scons should build dependencies"
+      echo ""
+      echo "  --scons-extra-args=${SCONS_EXTRA_ARGS}"
+      echo "    Optional args to pass to DAOS scons"
+      exit 0
+      ;;
+    --build-dir*|-d*) if [[ "$1" != *=* ]]; then shift; fi
+      BUILD_DIR="${1#*=}";;
+    --daos-branch*|-b*) if [[ "$1" != *=* ]]; then shift; fi
+      DAOS_BRANCH="${1#*=}";;
+    --daos-commit*) if [[ "$1" != *=* ]]; then shift; fi
+      DAOS_COMMIT="${1#*=}";;
+    --extra-daos-branches*) if [[ "$1" != *=* ]]; then shift; fi
+      EXTRA_DAOS_BRANCHES=(${1#*=});;
+    --extra-daos-cherry*) if [[ "$1" != *=* ]]; then shift; fi
+      EXTRA_DAOS_CHERRY=(${1#*=});;
+    --daos-build-type*) if [[ "$1" != *=* ]]; then shift; fi
+      DAOS_BUILD_TYPE="${1#*=}";;
+    --mpi-target*) if [[ "$1" != *=* ]]; then shift; fi
+      MPI_TARGET="${1#*=}";;
+    --ior-commit*) if [[ "$1" != *=* ]]; then shift; fi
+      IOR_COMMIT="${1#*=}";;
+    --mpich-dir*) if [[ "$1" != *=* ]]; then shift; fi
+      export MPICH_DIR="${1#*=}";;
+    --openmpi-dir*) if [[ "$1" != *=* ]]; then shift; fi
+      export OPENMPI_DIR="${1#*=}";;
+    --scons-extra-args*) if [[ "$1" != *=* ]]; then shift; fi
+      SCONS_EXTRA_ARGS="${1#*=}";;
+    *)
+      >&2 printf "Invalid argument: $1\n"
+      exit 1
+      ;;
+  esac
+  shift
+done
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-TIMESTAMP=$(date +%Y%m%d)
 LATEST_DAOS=${BUILD_DIR}/${TIMESTAMP}/daos/install
 
-export PATH=~/.local/bin:$PATH
-export PYTHONPATH=$PYTHONPATH:~/.local/lib
-
-source ${CURRENT_DIR}/build_env.sh ${MPI_TARGET} || exit
-
-declare -a PRECIOUS_FILES=("bin/daos"
-                           "bin/daos_server"
-                           "bin/daos_agent"
-                           "bin/dmg"
-                           "ior${MPI_SUFFIX}/bin/${IOR_BIN}"
-                           "ior${MPI_SUFFIX}/bin/${MDTEST_BIN}"
-                           )
+# Setup the environment
+function setup_env() {
+  module unload impi pmix hwloc || return
+  export PATH=~/.local/bin:$PATH
+  export PYTHONPATH=$PYTHONPATH:~/.local/lib
+  source ${CURRENT_DIR}/build_env.sh ${MPI_TARGET} || return
+  module list || return
+}
 
 # Check if git HEAD contains a specified commit
 function git_has_commit() {
@@ -68,7 +119,9 @@ function git_has_commit() {
 # Merge a "hack" branch and user-specified branches
 function merge_extra_daos_branches() {
   local hack_branch=""
-  if $(git_has_commit "f15d6c9") = true; then
+  if $(git_has_commit "1650544") = true; then
+    hack_branch="origin/dbohning-io500-base-1650544-2.0"
+  elif $(git_has_commit "f15d6c9") = true; then
     hack_branch="origin/dbohning-io500-base-f15d6c9"
   elif $(git_has_commit "b7a8e51") = true; then
     hack_branch="origin/dbohning-io500-base-b7a8e51-2.0"
@@ -80,16 +133,14 @@ function merge_extra_daos_branches() {
     hack_branch="origin/dbohning-io500-base-40f8636"
   elif $(git_has_commit "daaf038") = true; then
     hack_branch="origin/dbohning-io500-base-daaf038"
-  elif $(git_has_commit "5d740e5") = true; then
-    hack_branch="origin/dbohning-io500-base-5d740e5"
   else
     hack_branch="origin/mjmac/io500-frontera"
   fi;
 
   # Add the hack branch to the user-specified branches
-  DAOS_BRANCHES+=("${hack_branch}")
+  EXTRA_DAOS_BRANCHES+=("${hack_branch}")
 
-  for PATCH in "${DAOS_BRANCHES[@]}"
+  for PATCH in "${EXTRA_DAOS_BRANCHES[@]}"
   do
     echo "Merging branch: ${PATCH}"
     git log -n 1 --pretty=format:"commit %H%n" "${PATCH}" || return
@@ -99,7 +150,7 @@ function merge_extra_daos_branches() {
 }
 
 function cherry_pick_daos_commits() {
-  for COMMIT in "${DAOS_CHERRY[@]}"
+  for COMMIT in "${EXTRA_DAOS_CHERRY[@]}"
   do
     echo "Cherry-picking commit: ${COMMIT}"
     git log -n 1 --pretty=format:"commit %H%n" "${COMMIT}" || return
@@ -114,25 +165,6 @@ function print_repo_info() {
   echo "Repo: ${REPO_NAME}"
   git log -n 1 --pretty=format:"commit %H%n"
   echo
-}
-
-function check_target_files_exist() {
-  for i in "${PRECIOUS_FILES[@]}"
-  do
-    CURRENT_FILE=${LATEST_DAOS}/${i}
-
-    if [ ! -f ${CURRENT_FILE} ]; then
-      echo "Error: missing file ${CURRENT_FILE}"
-      exit 1
-    fi
-  done
-}
-
-function check_daos_linkage() {
-  if ! ldd ${1} | grep -q libdaos.so ; then
-    echo "Error: file ${1} does not link DAOS libraries"
-    exit 1
-  fi
 }
 
 function install_python_deps() {
@@ -152,6 +184,110 @@ function install_python_deps() {
     eval ${cmd} || return
 }
 
+# Setup a new build directory, removing if it already exists
+function setup_build_dir() {
+  rm -rf ${BUILD_DIR}/${TIMESTAMP}
+  mkdir -p ${BUILD_DIR}/${TIMESTAMP}
+  rm -f ${BUILD_DIR}/latest
+  ln -s ${BUILD_DIR}/${TIMESTAMP} ${BUILD_DIR}/latest
+}
+
+# Clone, checkout, and merge DAOS branches
+function clone_daos() {
+  pushd ${BUILD_DIR}/${TIMESTAMP}
+  git clone https://github.com/daos-stack/daos.git -b "${DAOS_BRANCH}"
+  pushd daos
+  if [ ! -z "${DAOS_COMMIT}" ]; then
+      git checkout -b "${DAOS_COMMIT}" "${DAOS_COMMIT}"
+  fi
+  git submodule init
+  git submodule update
+  print_repo_info |& tee -a ${BUILD_DIR}/${TIMESTAMP}/repo_info.txt
+  merge_extra_daos_branches |& tee -a ${BUILD_DIR}/${TIMESTAMP}/repo_info.txt
+  cherry_pick_daos_commits |& tee -a ${BUILD_DIR}/${TIMESTAMP}/repo_info.txt
+  popd
+  popd
+}
+
+# Build DAOS
+function build_daos() {
+  pushd ${BUILD_DIR}/${TIMESTAMP}/daos
+  scons MPI_PKG=any \
+        --build-deps=${SCONS_BUILD_DEPS} \
+        --config=force \
+        BUILD_TYPE=${DAOS_BUILD_TYPE} \
+        install ${SCONS_EXTRA_ARGS}
+  popd
+}
+
+
+# Clone and checkout IOR and MDTest
+function clone_ior_mdtest() {
+  pushd ${BUILD_DIR}/${TIMESTAMP}
+  git clone https://github.com/hpc/ior.git
+  pushd ${BUILD_DIR}/${TIMESTAMP}/ior
+  if [ ! -z "${IOR_COMMIT}" ]; then
+      git checkout -b "${IOR_COMMIT}" "${IOR_COMMIT}"
+  fi
+
+  # Apply patch to make all processes write the same data.
+  # This is needed to reduce pool space usage so a larger number of clients can be used.
+  cp ${CURRENT_DIR}/patches/ior_dedup_workaround.patch .
+  git apply ior_dedup_workaround.patch
+
+  print_repo_info |& tee -a ${BUILD_DIR}/${TIMESTAMP}/repo_info.txt
+  ./bootstrap
+  popd
+  popd
+}
+
+# Build IOR and MDTest
+function build_ior_mdtest() {
+  pushd ${BUILD_DIR}/${TIMESTAMP}/ior
+  ./configure --prefix=${LATEST_DAOS}/ior${MPI_SUFFIX} \
+              MPICC=${MPI_BIN}/mpicc \
+              --with-daos=${LATEST_DAOS} \
+              CPPFLAGS=-I${LATEST_DAOS}/prereq/release/mercury/include \
+              LIBS=-lmpi
+  make clean
+  make
+  make install
+
+  pushd ${LATEST_DAOS}/ior${MPI_SUFFIX}/bin
+  mv -v ior ${IOR_BIN}
+  mv -v mdtest ${MDTEST_BIN}
+  popd
+  popd
+}
+
+# Perform a basic revision of the built binaries
+function check_target_files_exist() {
+  declare -a PRECIOUS_FILES=("bin/daos"
+                             "bin/daos_server"
+                             "bin/daos_agent"
+                             "bin/dmg"
+                             "ior${MPI_SUFFIX}/bin/${IOR_BIN}"
+                             "ior${MPI_SUFFIX}/bin/${MDTEST_BIN}"
+                             )
+  for i in "${PRECIOUS_FILES[@]}"
+  do
+    CURRENT_FILE=${LATEST_DAOS}/${i}
+
+    if [ ! -f ${CURRENT_FILE} ]; then
+      echo "Error: missing file ${CURRENT_FILE}"
+      exit 1
+    fi
+  done
+}
+
+function check_daos_linkage() {
+  local path=${1}
+  if ! ldd ${path} | grep -q libdaos.so ; then
+    echo "Error: file ${path} does not link DAOS libraries"
+    exit 1
+  fi
+}
+
 # Exit if any command fails
 function check_retcode(){
   exit_code=${1}
@@ -162,73 +298,24 @@ function check_retcode(){
     exit ${exit_code}
   fi
 }
-trap 'check_retcode $? ${BASH_COMMAND}' EXIT
-set -e
-set -o pipefail
 
-install_python_deps
+function main() {
+  trap 'check_retcode $? ${BASH_COMMAND}' EXIT
+  set -e
+  set -o pipefail
 
-rm -rf ${BUILD_DIR}/${TIMESTAMP}
-mkdir -p ${BUILD_DIR}/${TIMESTAMP}
+  setup_env
+  install_python_deps
+  setup_build_dir
+  clone_daos
+  build_daos
+  clone_ior_mdtest
+  build_ior_mdtest
 
-pushd $BUILD_DIR/
-rm -f latest
-ln -s ${BUILD_DIR}/${TIMESTAMP} latest
-pushd ${BUILD_DIR}/${TIMESTAMP}
-git clone https://github.com/daos-stack/daos.git -b "${DAOS_BRANCH}"
-pushd daos
-if [ ! -z "${DAOS_COMMIT}" ]; then
-    git checkout -b "${DAOS_COMMIT}" "${DAOS_COMMIT}"
-fi
-git submodule init
-git submodule update
-print_repo_info |& tee -a ${BUILD_DIR}/${TIMESTAMP}/repo_info.txt
-merge_extra_daos_branches |& tee -a ${BUILD_DIR}/${TIMESTAMP}/repo_info.txt
-cherry_pick_daos_commits |& tee -a ${BUILD_DIR}/${TIMESTAMP}/repo_info.txt
-scons MPI_PKG=any \
-      --build-deps=yes \
-      --config=force \
-      BUILD_TYPE=${DAOS_BUILD_TYPE} \
-      install ${EXTRA_BUILD}
-popd
-popd
-popd
+  check_target_files_exist
+  check_daos_linkage ${LATEST_DAOS}/ior${MPI_SUFFIX}/bin/${IOR_BIN}
+  check_daos_linkage ${LATEST_DAOS}/ior${MPI_SUFFIX}/bin/${MDTEST_BIN}
+}
 
+main
 
-# Build IOR/MDTEST
-
-pushd ${BUILD_DIR}/${TIMESTAMP}
-git clone https://github.com/hpc/ior.git
-pushd ${BUILD_DIR}/${TIMESTAMP}/ior
-if [ ! -z "${IOR_COMMIT}" ]; then
-    git checkout -b "${IOR_COMMIT}" "${IOR_COMMIT}"
-fi
-
-# Apply patch to make all processes write the same data.
-# This is needed to reduce pool space usage so a larger number of clients can be used.
-cp ${CURRENT_DIR}/patches/ior_dedup_workaround.patch .
-git apply ior_dedup_workaround.patch
-
-print_repo_info |& tee -a ${BUILD_DIR}/${TIMESTAMP}/repo_info.txt
-./bootstrap
-./configure --prefix=${LATEST_DAOS}/ior${MPI_SUFFIX} \
-            MPICC=${MPI_BIN}/mpicc \
-            --with-daos=${LATEST_DAOS} \
-            CPPFLAGS=-I${LATEST_DAOS}/prereq/release/mercury/include \
-            LIBS=-lmpi
-make clean
-make
-make install
-
-pushd ${LATEST_DAOS}/ior${MPI_SUFFIX}/bin
-mv -v ior ${IOR_BIN}
-mv -v mdtest ${MDTEST_BIN}
-popd
-
-popd
-popd
-
-# Perform a basic revision of the built binaries
-check_target_files_exist
-check_daos_linkage ${LATEST_DAOS}/ior${MPI_SUFFIX}/bin/${IOR_BIN}
-check_daos_linkage ${LATEST_DAOS}/ior${MPI_SUFFIX}/bin/${MDTEST_BIN}
