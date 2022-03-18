711c794be3fbc44c97b14b35eabbf4067bfc0d7e
Dalton Bohning <dalton.bohning@intel.com>
2022-03-18 10:24:30 -0700

    Add tests/max/max.py (#120)
    Maximum configuration with ior easy
    
    Signed-off-by: Dalton Bohning <dalton.bohning@intel.com>

---
+++ frontera/tests/max/max.py
@@ -0,0 +1,31 @@
+'''
+    Max configuration.
+    Defines a list 'tests' containing dictionary items of tests.
+'''
+
+# Default environment variables used by each test
+env_vars = {
+    'pool_size': '85G',
+    'chunk_size': '1M',
+    'segments': '1',
+    'xfer_size': '1M',
+    'block_size': '150G',
+    'sw_time': '60',
+    'iterations': '1',
+    'ppc': 8
+}
+
+# List of tests
+tests = [
+    {
+        'test_group': 'IOR',
+        'test_name': 'ior_easy',
+        'oclass': 'SX',
+        'scale': [
+            # (num_servers, num_clients, timeout_minutes)
+            (128, 256, 10),
+        ],
+        'env_vars': dict(env_vars),
+        'enabled': True
+    },
+]
