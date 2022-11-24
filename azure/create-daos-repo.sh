6b2756682ba53b843201748e44a2d2c5dffb5aa6
Cedric Koch-Hofer <cedric.koch.hofer@gmail.com>
2022-11-24 12:57:58 +0000

    DAOS-10749 azure: DAOS master create repo
    Add script allowing to create a repo with latest DAOS dependencies.
    
    Signed-off-by: Cedric Koch-Hofer <cedric.koch-hofer@intel.com>

---
+++ azure/create-daos-repo.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# set -x
+set -e -o pipefail
+
+CWD="$(realpath "$(dirname $0)")"
+
+DAOS_PKGS=(
+	argobots
+	compat-hwloc1
+	daos
+	daos-admin
+	daos-admin-debuginfo
+	daos-client
+	daos-client-debuginfo
+	daos-client-tests
+	daos-client-tests-debuginfo
+	daos-client-tests-openmpi
+	daos-client-tests-openmpi-debuginfo
+	daos-debuginfo
+	daos-debugsource
+	daos-devel
+	daos-firmware
+	daos-firmware-debuginfo
+	daos-mofed-shim
+	daos-serialize
+	daos-serialize-debuginfo
+	daos-server
+	daos-server-debuginfo
+	daos-server-tests
+	daos-server-tests-debuginfo
+	daos-tests
+	daos-tests-internal
+	dpdk
+	hdf5-mpich
+	libfabric
+	libisa-l
+	libisa-l_crypto
+	libpmem
+	libpmemobj
+	libpmempool
+	librpmem
+	mercury
+	spdk
+	spdk-tools
+)
+
+mkdir daos-master-el8
+pushd "$PWD/daos-master-el8" &>/dev/null
+dnf download -y ${DAOS_PKGS[@]}
+popd &>/dev/null
+tar cvJf daos-master-el8.txz daos-master-el8
+rm -fr daos-master-el8
