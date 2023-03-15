1fb2f9eb531299cd408fbbca409abd40e1a51191
Cedric Koch-Hofer <cedric.koch.hofer@gmail.com>
2023-03-15 14:34:22 +0000

    DAOS-10749 azure: Add backup with Azure Blob
    Update backup script to use Azure blob for storing backup
    
    Signed-off-by: Cedric Koch-Hofer <cedric.koch-hofer@intel.com>

--- azure/backup-core_log.sh
+++ azure/backup-core_log.sh
@@ -7,9 +7,22 @@ CWD="$(realpath "$(dirname $0)")"
 
 source "$CWD/envs/env.sh"
 
-set -x
+BLOB_DIR="$HOME/backup/blobfuse-ckochhof"
+if ! findmnt -t fuse -S blobfuse -M "$BLOB_DIR" ; then
+	blobfuse $BLOB_DIR \
+		--tmp-path=/mnt/blobfusetmp \
+		--config-file=/home/azureuser/local/share/blobfuse/fuse_connection.cfg \
+		 --use-adls=true \
+		-o attr_timeout=240 \
+		-o entry_timeout=240 \
+		-o negative_timeout=120
+fi
+
+BACKUP_DIR="$BLOB_DIR/$TIMESTAMP"
 for hostname in $(nodeset -e $SERVER_NODES) ; do
-	mkdir -p "$CWD/debug/$TIMESTAMP/cores/$hostname" "$CWD/debug/$TIMESTAMP/logs/$hostname"
-	scp $hostname:"/tmp/daos_*.log" "$CWD/debug/$TIMESTAMP/logs/$hostname"
-	# ssh $hostname sudo tar -C /var/lib/systemd/coredump/ -c . | tar -C "$CWD/debug/$TIMESTAMP/cores/$hostname" -xv
+	mkdir -p "$BACKUP_DIR/cores/$hostname" "$BACKUP_DIR/logs/$hostname"
+	echo "[INFO] Backup logs of server $hostname"
+	scp $hostname:"/tmp/daos_*.log" "$BACKUP_DIR/logs/$hostname"
+	echo "[INFO] Backup cores of server $hostname"
+	ssh $hostname sudo tar -C /var/lib/systemd/coredump/ -c . | tar -C "$BACKUP_DIR/cores/$hostname" -xv
 done
