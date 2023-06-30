83e94a3ea6fe10f831e41fe04b969815a3add78f
James Nunez <jamesanunez@users.noreply.github.com>
2023-06-30 15:29:15 -0600

    endeavour: collect common testlist functions (#167)
    The testlist/testlist_<app_name>.sh scripts start and run
    application app_name.  Each of these scripts have common code to parse
    and check the command line parameters. Collect these common functions
    into a new script that can be sourced by all testlist application
    scripts.
    
    Signed-off-by: James Nunez <james.nunez@intel.com>

--- edv/test_scripts/client/testlists/testlist_efispec.sh
+++ edv/test_scripts/client/testlists/testlist_efispec.sh
@@ -1,66 +1,58 @@
 #!/bin/sh
 
+# Get the common routines to parse command line
+source ${RUNDIR}/testlists/testlist_common.sh
+
+# Get the global environment variables
+source ${RUNDIR}/scripts/client_env.sh
+
+# Parse the command line
+parseAndSetParameters $@
+
 export SCM="700G" #700G
 export NVME="30T" #1T
 
 export SRV_HEADNODE="edaos09"
 export MPI="IMPI"
-export RUNDIR="/panfs/users/schan15/client"
-export BUILDDIR="/panfs/users/schan15/builds"
-export TB="daos_rel201rc1"
+export RUNDIR="/panfs/users/${USER}/client"
+export BUILDDIR="/panfs/users/${USER}/builds"
 export MOUNTDIR="/tmp/daos"
-export APPSRC="/panfs/users/schan15/apps/${MPI}/efi_johann"
+export APPSRC="/panfs/users/${USER}/apps/${MPI}/efi_johann"
 export APPNAME="efispec"
 export APPRUNDIR="${MOUNTDIR}/efi_johann/test"
 export OUTDIR="${MOUNTDIR}/efi_johann/test"
 export APPINFILEDEST="${APPRUNDIR}"
-export APPINFILE="/panfs/users/schan15/client/input/e2vp2.cfg.big"
+export APPINFILE="/panfs/users/${USER}/client/input/e2vp2.cfg.big"
 
 export TESTCMD="${MOUNTDIR}/efi_johann/bin/efispec3d_1.0_avx512_async.exe"
 
-export PPN=6 #64 for big
 export MOUNT_DFUSE=1
 
 cd $RUNDIR
 
 export I_MPI_JOB_STARTUP_TIMEOUT=7200
 
-#FPP
+# EFISPEC always writes FPP
 export DFUSECACHE="--disable-caching"
 unset ROMIO_FSTYPE_FORCE
-export IL=1
-export RF=0
-export TEST="${MPI}_EFISPEC_FPP_c16_noilstream"
-export NSERVER="32"
-export NCLIENT="16"
-export RANKS="0-$((${NSERVER}-1))"
-export TESTDIR="${TEST}_${NSERVER}e_${NCLIENT}c_${PPN}ppn_RF${RF}_IL${IL}"
-export RESULTDIR="${RUNDIR}/results/${TESTDIR}"
-export RUNLOG="${TESTDIR}.log"
+export TEST="${MPI}_EFISPEC_FPP"
 
-rm -rf ${RESULTDIR}
-mkdir -p ${RESULTDIR}
+# Remove commas from the comma separated list of servers
+server_list=$(echo "$server_list" | sed 's/,/ /g')
 
-${RUNDIR}/scripts/run_client.sh 2>&1 | tee ${RESULTDIR}/${RUNLOG}
+for NSERVER in ${server_list}
+do
+  export NSERVER=$NSERVER
+  export RANKS="0-$((${NSERVER}-1))"
+  export TESTDIR="${TEST}_${NSERVER}e_${NCLIENT}c_${PPN}ppn_RF${RF}_IL${IL}"
+  export RESULTDIR="${RUNDIR}/results/${TESTDIR}"
+  export RUNLOG="${TESTDIR}.log"
 
-#sleep 15
-#
-#export DFUSECACHE="--enable-caching"
-#unset ROMIO_FSTYPE_FORCE
-#export IL=0
-#export RF=0
-#export TEST="${MPI}_EFISPEC_FPP_c16_noilstream"
-#export NSERVER="32"
-#export NCLIENT="16"
-#export RANKS="0-$((${NSERVER}-1))"
-#export TESTDIR="${TEST}_${NSERVER}e_${NCLIENT}c_${PPN}ppn_RF${RF}_IL${IL}"
-#export RESULTDIR="${RUNDIR}/results/${TESTDIR}"
-#export RUNLOG="${TESTDIR}.log"
-#
-#rm -rf ${RESULTDIR}
-#mkdir -p ${RESULTDIR}
-#
-#${RUNDIR}/scripts/run_client.sh 2>&1 | tee ${RESULTDIR}/${RUNLOG}
-#
-#sleep 15
+  rm -rf ${RESULTDIR}
+  mkdir -p ${RESULTDIR}
+
+  ${RUNDIR}/scripts/run_client.sh 2>&1 | tee ${RESULTDIR}/${RUNLOG}
+
+  sleep 15
+done
 
